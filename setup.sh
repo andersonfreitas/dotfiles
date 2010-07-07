dc0b90a2ce26c1674ab99e3a58eddc1c94df150b
Anderson Freitas <anderson.amf@gmail.com>
2010-07-07 00:31:39 -0300

    Instalador setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
+ln -s ~/bin/dotfiles/bashrc ~/.bashrc
+ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
+
+git config --global user.name "Anderson Freitas"
+git config --global user.email "anderson.amf@gmail.com"
+#git config --global core.excludesfile "~/.gitignore"
