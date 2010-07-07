967dbe2134135beb887d3d19aecd7a390ffd340b
Anderson Freitas <anderson.amf@gmail.com>
2010-07-07 00:37:12 -0300

    configuração do SSH no setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
+ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
 ln -s ~/bin/dotfiles/bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
 
