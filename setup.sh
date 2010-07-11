9f43490561006a6daff30fc2651fe97beae3ee34
Anderson Freitas <anderson.amf@gmail.com>
2010-07-11 19:44:39 -0300

    Removed email address to avoid spam

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ ln -s ~/bin/dotfiles/bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
 
 git config --global user.name "Anderson Freitas"
-git config --global user.email "anderson.amf@gmail.com"
+git config --global user.email "###@###.com"
 #git config --global core.excludesfile "~/.gitignore"
