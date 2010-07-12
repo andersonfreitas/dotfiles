8cd8fa6a84f7553a27f3c70af069ca3fecf269fb
Anderson Freitas <anderson.amf@gmail.com>
2010-07-12 09:15:10 -0300

    new git global aliases

--- setup.sh
+++ setup.sh
@@ -5,4 +5,9 @@ ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
 
 git config --global user.name "Anderson Freitas"
 git config --global user.email "###@###.com"
+git config --global alias.oneline "log --pretty=oneline"
+git config --global alias.co "checkout"
+git config --global alias.ci "commit"
+git config --global alias.br "branch"
+git config --global alias.la "log --pretty=\"format:%ad %h (%an): %s\" --date=short"
 #git config --global core.excludesfile "~/.gitignore"
