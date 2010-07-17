f52d2ef907a139e2e900596029011f271a0eea06
Anderson Freitas <anderson.amf@gmail.com>
2010-07-17 01:16:44 -0300

    Viewing git diff in Changes.app

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
 ln -s ~/bin/dotfiles/bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
+ln -s ~/bin/dotfiles/git-external-chdiff ~/bin/git-external-chdiff
 
 git config --global user.name "Anderson Freitas"
 git config --global user.email "###@###.com"
@@ -10,4 +11,6 @@ git config --global alias.co "checkout"
 git config --global alias.ci "commit"
 git config --global alias.br "branch"
 git config --global alias.la "log --pretty=\"format:%ad %h (%an): %s\" --date=short"
+git config --global diff.external git-external-chdiff
 #git config --global core.excludesfile "~/.gitignore"
+
