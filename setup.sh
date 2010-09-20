d26c5d985d085f13df3394cca99afbbc6cce84db
Anderson Freitas <anderson.amf@gmail.com>
2010-09-20 19:44:55 -0300

    Added screen config as http://blog.anassina.com/28545646

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
 ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
 ln -s ~/bin/dotfiles/bashrc ~/.bashrc
+ln -s ~/bin/dotfiles/screenrc ~/.screenrc
 ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
 ln -s ~/bin/dotfiles/git-external-chdiff ~/bin/git-external-chdiff
 
