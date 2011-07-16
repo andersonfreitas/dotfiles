f113d305ec84bc28f6187e8cbc9ac3affca4d64e
Anderson Freitas <anderson.amf@gmail.com>
2011-07-16 14:25:27 -0300

    organizing...

--- setup.sh
+++ setup.sh
@@ -4,4 +4,28 @@ ln -s ~/bin/dotfiles/bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/screenrc ~/.screenrc
 ln -s ~/bin/dotfiles/bashrc ~/.bash_profile
 
+# ========
+# = TODO =
+# ========
+
+# function relink() {
+#   if [[ -h "$1" ]]; then
+#     echo "Relinking $1"
+#     # Symbolic link? Then recreate.
+#     rm "$1"
+#     ln -sn "$2" "$1"
+#   elif [[ ! -e "$1" ]]; then
+#     echo "Linking $1"
+#     ln -sn "$2" "$1"
+#   else
+#     echo "$1 exists as a real file, skipping."
+#   fi
+# }
+# 
+# cd ~
+# relink .bash_profile ~/.dotfiles/bash_profile
+# relink .bashrc ~/.dotfiles/bashrc
+# relink .gitconfig ~/.dotfiles/git-config
+# relink .gitignore ~/.dotfiles/git-ignore-global
+# relink bin ~/.dotfiles/bin
 
