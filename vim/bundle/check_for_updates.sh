991df99f471ba8ea8c97ee12b100619ec9288dc8
Anderson Freitas <anderson.amf@gmail.com>
2011-08-22 17:26:32 -0300

    Adding utility to update the submodules

---
+++ vim/bundle/check_for_updates.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+oldcwd=$(pwd)
+
+cd "$(dirname "$0")"
+for submodule in *; do
+	if [ -d "$submodule/.git" ]; then
+		cd "$submodule"
+		echo "--- $submodule:"
+		git co master
+		git pull
+		cd ..
+	fi
+done
+
+cd "$oldcwd"
