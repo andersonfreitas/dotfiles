427c17949cc0862bcbd3c167390ba9086e100e41
Anderson Freitas <anderson.amf@gmail.com>
2012-09-05 16:33:23 -0300

    coreutils

--- bash/env.bash
+++ bash/env.bash
@@ -7,7 +7,9 @@ export GIT_EDITOR="vim"
 #export EDITOR='mvim -f -c "au VimLeave * \!open -a iTerm"'
 
 export PATH=~/bin:/usr/local/bin:/usr/local/sbin:$PATH
-export PATH=$PATH
+# export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+export PATH="$(brew --repository)/Library/LinkedKegs/coreutils/libexec/gnubin:$PATH"
+
 export HISTSIZE=10000
 export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}"; echo -ne "\007"'
 export PAGER='less' # less is more (than more)
